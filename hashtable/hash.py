cf4b85200ba4042c3b7da578d891693d0dee4e3c
hamdi elhamdi <hamdielhamdi@outlook.com>
2021-07-14 17:26:22 +0100

    add hashtable

--- hashtable/hash.py
+++ hashtable/hash.py
@@ -1,7 +1,8 @@
+
 class HashTable:  
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
         
     def get_hash(self, key):
         hash = 0
@@ -9,32 +10,42 @@ class HashTable:
             hash += ord(char)
         return hash % self.MAX
     
-    def __getitem__(self, index):
-        h = self.get_hash(index)
-        return self.arr[h]
-    
+    def __getitem__(self, key):
+        arr_index = self.get_hash(key)
+        for kv in self.arr[arr_index]:
+            if kv[0] == key:
+                return kv[1]
+            
     def __setitem__(self, key, val):
         h = self.get_hash(key)
-        self.arr[h] = val    
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element)==2 and element[0] == key:
+                self.arr[h][idx] = (key,val)
+                found = True
+        if not found:
+            self.arr[h].append((key,val))
         
     def __delitem__(self, key):
-        h = self.get_hash(key)
-        self.arr[h] = None
+        arr_index = self.get_hash(key)
+        for index, kv in enumerate(self.arr[arr_index]):
+            if kv[0] == key:
+                print("del",index)
+                del self.arr[arr_index][index]
 
     def get_all(self):
-    	return self.arr
+        return self.arr
 # test 
 
 h = HashTable()
 
-h['first1'] = 0
+h['march 6'] = 0
 
-h['first2'] = 2
+h['march 17'] = 5
 
-h['first3'] = 3
-
-h['first4'] = 4
-
+h['march 1'] = 2
+h['march 71'] = 2
 
 print(h.get_all())
-print(h['first3'])
+
+
